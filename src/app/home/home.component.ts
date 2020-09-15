369a6c866e1fcce02a61c414074f0ea7363d7f8c
Kumar Bandaru <swamykumara544@gmail.com>
2020-09-15 17:22:37 +0530

    added headers in post call

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,7 +4,7 @@ import { first } from 'rxjs/operators';
 import { User } from '../model/user';
 import {AuthinticationService} from '../services/authintication.service';
 import {UserService} from '../services/user.service';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -13,18 +13,24 @@ import {UserService} from '../services/user.service';
 export class HomeComponent implements OnInit {
   currentUser: User;
   users = [];
-
+  postId:any;
   constructor(
       private authenticationService: AuthinticationService,
-      private userService: UserService
+      private userService: UserService,
+      private http: HttpClient
   ) {
       this.currentUser = this.authenticationService.currentUserValue;
   }
 
   ngOnInit() {
       this.loadAllUsers();
-  }
+      const headers = { 'Authorization': 'Bearer my-token', 'My-Custom-Header': 'foobar' }
+const body = { title: 'Angular POST Request Example' }
+this.http.post<any>('https://jsonplaceholder.typicode.com/posts', body, { headers }).subscribe(data => {
+    this.postId = data.id;
+})
 
+  }
   deleteUser(id: number) {
       this.userService.delete(id)
           .pipe(first())
